def7f86cc9dec4343a4bb1d61cbe15eb7dfd822e
Mark1U <mark@zurcloud.com>
2024-10-02 15:26:29 +0200

    TS Tax Calculator

---
+++ src/main.ts
@@ -0,0 +1,78 @@
+import './style.css'
+
+const calcBtn = document.querySelector('#calcBtn') as HTMLInputElement;
+const resultVAT = document.querySelector('#resultVAT') as HTMLElement;
+const resultGross = document.querySelector('#resultGross') as HTMLElement;
+
+let isToNetto: boolean = false;
+let rate: number = 0.19;
+
+const calcTaxResult = (): void => {
+
+  const amountInput = document.querySelector('#amount') as HTMLInputElement;
+
+  let amount: number
+  if (amountInput) {
+    amount = parseFloat(amountInput.value.replace(',', '.'));
+
+    let vat: number;
+    let gross: number;
+
+    if (!isToNetto) {
+      console.log('Berechne Netto->Brutto');
+
+      gross = amount * (1 + rate);
+      vat = gross - amount;
+    } else {
+      console.log('Berechne Brutto->Netto');
+
+      gross = (amount / (1 + rate));
+      vat = amount - gross;
+    }
+
+    console.log({ amount, rate, vat, gross });
+
+    if (resultGross && resultVAT) {
+      resultVAT.textContent = vat.toFixed(2) + " €";
+      resultGross.textContent = gross.toFixed(2) + " €";
+    }
+  }
+}
+
+if (calcBtn) {
+  calcBtn.addEventListener('click', e => {
+    console.log('Berchnung gestartet.');
+    e.preventDefault();
+    calcTaxResult();
+  })
+}
+
+const radioBtnList = document.querySelectorAll('input[type="radio"]') as NodeListOf<HTMLElement>;
+
+if (radioBtnList.length > 0) {
+  console.log({ radioBtnList });
+  for (let i = 0; i < radioBtnList.length; i++) {
+    radioBtnList[i].addEventListener('click', (e) => {
+      console.log(e.type);
+
+      const amountLabel = document.querySelector('#amountLabel ') as HTMLElement;
+      const rateInput = document.querySelector('#rate7') as HTMLInputElement;
+      const taxDirection = document.querySelector('#brutto2netto') as HTMLInputElement;
+
+      if (taxDirection && !taxDirection.checked) {
+        isToNetto = false;
+        amountLabel.textContent = "Nettobetrag (Preis ohne Mehrwertsteuer) in Euro";
+      } else {
+        isToNetto = true;
+        amountLabel.textContent = "Bruttobetrag (Preis inklusive Mehrwertsteuer) in Euro";
+      }
+      (rateInput && rateInput.checked) ? rate = 0.07 : rate = 0.19;
+
+      console.log({ taxDirection, isToNetto, rate });
+
+      resultVAT.textContent = "€";
+      resultGross.textContent = "€";
+    });
+  }
+}
+
